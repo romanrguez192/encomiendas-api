60a3b18bbaaa907adc911071bc1df77676803dee
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2022-01-19 16:50:00 -0400

    Mutation recarga

--- src/schema/recarga.js
+++ src/schema/recarga.js
@@ -13,6 +13,26 @@ const Recarga = gql`
     fecha: Date!
     cliente: Cliente!
   }
+
+  extend type Mutation {
+    createRecarga(recarga: RecargaInput!): Recarga
+    updateRecarga(id: Int!, recarga: RecargaUpdateInput!): Recarga
+    deleteRecarga(id: Int!): Recarga
+  }
+
+  input RecargaInput {
+    precio: Float!
+    fecha: Date!
+    saldo: Float!
+    cedulaCliente: String!
+  }
+
+  input RecargaUpdateInput {
+    precio: Float
+    fecha: Date
+    saldo: Float
+    cedulaCliente: String
+  }
 `;
 
 const recargaResolvers = {
@@ -22,6 +42,28 @@ const recargaResolvers = {
     },
     recarga: (_parent, args, context) => {
       return context.prisma.recarga.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+    },
+  },
+  Mutation: {
+    createRecarga: (_parent, args, context) => {
+      return context.prisma.recarga.create({
+        data: args.recarga,
+      });
+    },
+    updateRecarga: (_parent, args, context) => {
+      return context.prisma.recarga.update({
+        where: {
+          id: args.id,
+        },
+        data: args.recarga,
+      });
+    },
+    deleteRecarga: (_parent, args, context) => {
+      return context.prisma.recarga.delete({
         where: {
           id: args.id,
         },
