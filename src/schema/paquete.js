0077587301285fa33e25db759f4f99b57d03d6ef
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2022-01-21 12:29:31 -0400

    Mutation paquete

--- src/schema/paquete.js
+++ src/schema/paquete.js
@@ -18,6 +18,34 @@ const Paquete = gql`
     encomienda: Encomienda!
     articulos: [Articulo]!
   }
+
+  extend type Mutation {
+    createPaquete(paquete: PaqueteInput!): Paquete
+    updatePaquete(id: Int!, paquete: PaqueteUpdateInput!): Paquete
+    deletePaquete(id: Int!): Paquete
+  }
+
+  input PaqueteInput {
+    empaquetado: Boolean!
+    peso: Int!
+    alto: Int
+    ancho: Int
+    profundidad: Int
+    fragil: Boolean!
+    tarifa: Float
+    idEncomienda: Int!
+  }
+
+  input PaqueteUpdateInput {
+    empaquetado: Boolean
+    peso: Int
+    alto: Int
+    ancho: Int
+    profundidad: Int
+    fragil: Boolean
+    tarifa: Float
+    idEncomienda: Int
+  }
 `;
 
 const paqueteResolvers = {
@@ -27,6 +55,28 @@ const paqueteResolvers = {
     },
     paquete: (_parent, args, context) => {
       return context.prisma.paquete.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+    },
+  },
+  Mutation: {
+    createPaquete: (_parent, args, context) => {
+      return context.prisma.paquete.create({
+        data: args.paquete,
+      });
+    },
+    updatePaquete: (_parent, args, context) => {
+      return context.prisma.paquete.update({
+        where: {
+          id: args.id,
+        },
+        data: args.paquete,
+      });
+    },
+    deletePaquete: (_parent, args, context) => {
+      return context.prisma.paquete.delete({
         where: {
           id: args.id,
         },
