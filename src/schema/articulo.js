c3ec48815ec33957a9916fb1eb01ca6f6de29b66
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2022-01-21 12:56:09 -0400

    Mutation articulo

--- src/schema/articulo.js
+++ src/schema/articulo.js
@@ -7,11 +7,34 @@ const Articulo = gql`
   }
 
   type Articulo {
-    idPaquete: Int!
     numero: Int!
     cantidad: Int!
     descripcion: String!
     paquete: Paquete!
+  }
+
+  extend type Mutation {
+    createArticulo(articulo: ArticuloInput!): Articulo
+    updateArticulo(
+      idPaquete: Int!
+      numero: Int!
+      articulo: ArticuloUpdateInput!
+    ): Articulo
+    deleteArticulo(idPaquete: Int!, numero: Int!): Articulo
+  }
+
+  input ArticuloInput {
+    idPaquete: Int!
+    numero: Int!
+    cantidad: Int!
+    descripcion: String!
+  }
+
+  input ArticuloUpdateInput {
+    idPaquete: Int
+    numero: Int
+    cantidad: Int
+    descripcion: String
   }
 `;
 
@@ -23,8 +46,38 @@ const articuloResolvers = {
     articulo: (_parent, args, context) => {
       return context.prisma.articulo.findUnique({
         where: {
-          idPaquete: args.idPaquete,
-          numero: args.numero,
+          idPaquete_numero: {
+            idPaquete: args.idPaquete,
+            numero: args.numero,
+          },
+        },
+      });
+    },
+  },
+  Mutation: {
+    createArticulo: (_parent, args, context) => {
+      return context.prisma.articulo.create({
+        data: args.articulo,
+      });
+    },
+    updateArticulo: (_parent, args, context) => {
+      return context.prisma.articulo.update({
+        where: {
+          idPaquete_numero: {
+            idPaquete: args.idPaquete,
+            numero: args.numero,
+          },
+        },
+        data: args.articulo,
+      });
+    },
+    deleteArticulo: (_parent, args, context) => {
+      return context.prisma.articulo.delete({
+        where: {
+          idPaquete_numero: {
+            idPaquete: args.idPaquete,
+            numero: args.numero,
+          },
         },
       });
     },
